226a5f5fc56d2f2b601448dc54d93ecf4cf022fa
VitiateXT <moritz.hoffmann@tutanota.com>
2022-08-07 14:40:30 +0200

    changed config.el

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -46,7 +46,7 @@
 (setq org-directory "~/org/")
 
 ;; set font+ fontsize
-(setq doom-font (font-spec :family "Ubuntu Mono" :size 32))
+(setq doom-font (font-spec :family "Inconsolata" :size 32))
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
 ;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
 ;;

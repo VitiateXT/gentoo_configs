32f3733ad5b31192db10d8d8bc5eaeac5a868f37
VitiateXT <moritz.hoffmann@tutanota.com>
2022-08-12 01:32:01 +0200

    init.el changed

--- init.el
+++ init.el
@@ -107,6 +107,18 @@
 (column-number-mode)
 (global-display-line-numbers-mode t)
 
+(use-package projectile
+  :diminish projectile-mode
+  :config (projectile-mode)
+  :custom ((projectile-completion-system 'ivy))
+  :init
+  (when (file-directory-p "~/code")
+    (setq projectile-project-search-path '("~/code")))
+  (setq projectile-switch-project-action #'projectile-dired))
+
+(use-package counsel-projectile
+  :config (counsel-projectile-mode))
+
 ;; Disable line numbers for some modes
 (dolist (mode '(org-mode-hook
                 term-mode-hook
@@ -137,7 +149,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(general use-package evil-tutor evil-collection doom-themes doom-modeline)))
+   '(counsel-projectile projectile general use-package evil-tutor evil-collection doom-themes doom-modeline)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

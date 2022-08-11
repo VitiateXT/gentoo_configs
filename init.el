fb80d72a4cd71bc04c0f69c2da607a46f2865618
VitiateXT <moritz.hoffmann@tutanota.com>
2022-08-11 19:36:26 +0200

    changed init.el

--- init.el
+++ init.el
@@ -17,7 +17,53 @@
   (package-install 'use-package))
 (setq use-package-always-ensure t)
 
-;; evil vi emulator
+(use-package general
+  :config
+  (general-evil-setup t)
+  (general-create-definer mrh/leader-keys
+    :keymaps '(normal insert visual emacs)
+    :prefix "SPC"))
+
+(nvmap :keymaps 'override :prefix "SPC"
+       "SPC"   '(counsel-M-x :which-key "M-x")
+       "c c"   '(compile :which-key "Compile")
+       "c C"   '(recompile :which-key "Recompile")
+       "h r r" '((lambda () (interactive) (load-file "~/.emacs.d/init.el")) :which-key "Reload emacs config")
+       "t t"   '(toggle-truncate-lines :which-key "Toggle truncate lines")
+       )
+
+(nvmap :keymaps 'override :prefix "SPC"
+       "m *"   '(org-ctrl-c-star :which-key "Org-ctrl-c-star")
+       "m +"   '(org-ctrl-c-minus :which-key "Org-ctrl-c-minus")
+       "m ."   '(counsel-org-goto :which-key "Counsel org goto")
+       "m e"   '(org-export-dispatch :which-key "Org export dispatch")
+       "m f"   '(org-footnote-new :which-key "Org footnote new")
+       "m h"   '(org-toggle-heading :which-key "Org toggle heading")
+       "m i"   '(org-toggle-item :which-key "Org toggle item")
+       "m n"   '(org-store-link :which-key "Org store link")
+       "m o"   '(org-set-property :which-key "Org set property")
+       "m t"   '(org-todo :which-key "Org todo")
+       "m x"   '(org-toggle-checkbox :which-key "Org toggle checkbox")
+       "m B"   '(org-babel-tangle :which-key "Org babel tangle")
+       "m I"   '(org-toggle-inline-images :which-key "Org toggle inline imager")
+       "m T"   '(org-todo-list :which-key "Org todo list")
+       "o a"   '(org-agenda :which-key "Org agenda")
+       )
+
+(nvmap :prefix "SPC"
+       "b b"   '(ibuffer :which-key "Ibuffer")
+       "b c"   '(clone-indirect-buffer-other-window :which-key "Clone indirect buffer other window")
+       "b k"   '(kill-current-buffer :which-key "Kill current buffer")
+       "b n"   '(next-buffer :which-key "Next buffer")
+       "b p"   '(previous-buffer :which-key "Previous buffer")
+       "b B"   '(ibuffer-list-buffers :which-key "Ibuffer list buffers")
+       "b K"   '(kill-buffer :which-key "Kill buffer")
+       "b f"   '(dired :which-key "find file")
+       "w n"   '(evil-window-new :whick-key "open new window")
+       "w c"   '(evil-window-delete :whick-key "close window")
+       )
+
+  ;; evil vi emulator
 (use-package evil
   :ensure t
   :init      ;; tweak evil's configuration before loading it
@@ -25,6 +71,8 @@
   (setq evil-want-keybinding nil)
   (setq evil-vsplit-window-right t)
   (setq evil-split-window-below t)
+  (setq evil-want-C-u-scroll t)
+  :config
   (evil-mode 1))
 
 
@@ -36,11 +84,6 @@
   (setq evil-collection-mode-list '(dashboard dired ibuffer))
   (evil-collection-init))
 (use-package evil-tutor)
-
-;; keybinds??
-(use-package general
-  :config
-  (general-evil-setup t))
 
 ;; fonts
 (set-face-attribute 'default nil
@@ -83,7 +126,6 @@
 (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
       doom-themes-enable-italic t) ; if nil, italics is universally disabled
 (load-theme 'doom-one t)
-;;(load-theme 'tommyh)
 
 ;; modeline
 (use-package doom-modeline

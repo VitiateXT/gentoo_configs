1433ef28d563d9d75b0b77bc97d755db81c37c41
VitiateXT <moritz.hoffmann@tutanota.com>
2023-01-27 10:31:26 +0100

    init.el changed

--- init.el
+++ init.el
@@ -5,13 +5,19 @@
 (setq inhibit-startup-message t)
 
 ;; set font
-(set-face-attribute 'default nil :font "Ubuntu Mono" :height 80)
+(set-face-attribute 'default nil :font "Ubuntu Mono" :height 90)
 
 ;; disable basic emacs tools
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 (menu-bar-mode -1)
 (set-fringe-mode 10)
+(setq-default tab-width 4)
+;; org babel enable clisp support
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((R . t)
+   (lisp . t)))
 
 ;; package sources
 (require 'package)
@@ -71,17 +77,44 @@
 	:config
 	(evil-mode 1))
 
+(use-package org-bullets
+      :ensure t
+      :config
+      (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
+
 ;; pdftools
 (use-package pdf-tools
-	     :ensure t)
+  :ensure t)
+
+(use-package slime
+  :ensure t)
+
+;; skeleton
+(define-skeleton org-skeleton
+  "Header info for a emacs-org file."
+  "Title: "
+  "#+TITLE:" str " \n"
+  "#+AUTHOR: Moritz R. Hoffmann\n"
+  "#+email: moritz.hoffmann@tutanota.com\n"
+  "#+INFOJS_OPT: \n"
+  "#+PROPERTY: session *R*" 
+  "#+PROPERTY: cache yes" 
+  "#+PROPERTY: results graphics" 
+  "#+PROPERTY: exports both"
+  "#+PROPERTY: tangle yes"
+  "#+STARTUP: indent \n"
+  "-------------------------------------------------------------------------------
+"
+  )
 
 ;; general keybinds
 (use-package general)
 
 (general-define-key 
-  :states '(normal motions visual emacs)
+  :states '(normal visual motion)
   :prefix "SPC" 
-  "h r r" '(lambda () (interactive) (load-file "~/.emacs.d/init.el")) :which-key "Reload emacs config"
+  "h r r" '(lambda () (interactive) (load-file "~/.emacs.d/init.el")) :which-key
+  "Reload emacs config"
   "b s"   'save-buffer :which-key "save buffer"
   "b c"   'clone-indirect-buffer-other-window :which-key "Clone indirect buffer other window"
   "b k"   'kill-current-buffer :which-key "Kill current buffer"
@@ -94,6 +127,14 @@
   "w c"   'evil-window-delete :whick-key "close window"
   "w r"   'evil-window-move-far-right :which-key "move window right"
   "w s"   'evil-window-vsplit :which-key "split window right"
+  "e b"   'eval-buffer :which-key "evaluate elisp buffer"
+  "l s"   'eval-last-sexp :which-key "evaluates last elisp s-expression"
+  "o s"   'shell :which-key "opens shell"
+  "o e"   'org-export-dispatch :which-key "opens org export"
+  "h h"   'org-insert-headind :which-key "org inserts headinf at same level"
+  "c b"   'org-insert-todo-heading :which-key "insert checkbox or TODO heading"
+  "t b"   'org-toggle-checkbox :which-key "toggle org checkbox"
+  "s k"   'org-skeleton :which-key "use org skeleton implementation"
   )
 
 
@@ -101,9 +142,9 @@
 (global-display-line-numbers-mode t)
 
 ;; Disable line numbers for some modes
-(dolist (mode '(org-mode-hook
-                term-mode-hook
-                eshell-mode-hook))
+(dolist (mode '(term-mode-hook
+                eshell-mode-hook
+		shell-mode-hook))
   (add-hook mode (lambda () (display-line-numbers-mode 0))))
 
 (custom-set-variables
@@ -112,7 +153,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(general evil which-key doom-modeline doom-themes all-the-icons ivy use-package)))
+   '(slime org-bullets general evil which-key doom-modeline doom-themes all-the-icons ivy use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

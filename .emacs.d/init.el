ddf7d37e943489809adee0b11d30c3cc93cc3def
VitiateXT <moritz.hoffmann@tutanota.com>
2023-02-18 00:06:29 +0100

    changed init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -4,6 +4,7 @@
 ;; no startup message
 (setq inhibit-startup-message t)
 
+(setq image-use-external-converter t)
 ;; set font
 (set-face-attribute 'default nil :font "Ubuntu Mono" :height 90)
 
@@ -20,7 +21,9 @@
 
 ;; package sources
 (require 'package)
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/"))
+(add-to-list 'package-archives '("nongnu" . "https://elpa.nongnu.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 
 ;; refresh
 (package-initialize)
@@ -33,6 +36,10 @@
 
 (require 'use-package)
 (setq use-package-always-ensure t)
+
+(require 'cl-lib)
+(use-package cl-libify
+  :ensure t)
 
 ;; ivy as completion framework
 (use-package ivy
@@ -83,15 +90,16 @@
   "#+TITLE:" str " \n"
   "#+AUTHOR: Moritz R. Hoffmann \n"
   "#+INFOJS_OPT: \n"
-  "#+PROPERTY: session *R*"
-  "#+PROPERTY: cache yes"
-  "#+PROPERTY: results graphics" 
-  "#+PROPERTY: exports both"
-  "#+PROPERTY: tangle yes"
+  "#+PROPERTY: session *R* \n"
+  "#+PROPERTY: cache yes \n"
+  "#+PROPERTY: results graphics \n" 
+  "#+PROPERTY: exports both \n"
+  "#+PROPERTY: tangle yes \n"
   "#+STARTUP: indent \n"
-  "#+LATEX_HEADER: \\usepackage{parskip}"
-  "#+LATEX_HEADER: \\usepackage[ngerman]{babel}"
-  "#+LANGUAGE: ngerman"
+  "#+STARTUP: content \n"
+  "#+LATEX_HEADER: \\usepackage{parskip} \n"
+  "#+LATEX_HEADER: \\usepackage[ngerman]{babel} \n"
+  "#+LANGUAGE: ngerman \n"
   "-------------------------------------------------------------------------------
 ")
 
@@ -109,8 +117,6 @@
 
 (setq TeX-PDF-mode t); PDF mode (rather than DVI-mode)
 
-(require 'cl-lib)
-
 (use-package slime
   :ensure t)
 
@@ -121,6 +127,8 @@
 (add-hook 'lisp-mode-hook (lambda () (slime-mode t)))
 (add-hook 'inferior-lisp-mode-hook (lambda () (inferior-slime-mode t)))
 (setq org-confirm-babel-evaluate nil)
+
+(setq org-src-tab-acts-natively t)
 
 (use-package org-bullets
       :ensure t
@@ -134,7 +142,31 @@
 ;; remember to download ess as gentoo package 
 (require 'ess)
 
-(use-package sudo-edit)
+(use-package sudo-edit
+  :ensure t)
+
+(use-package evil-collection
+  :ensure t
+  :after evil
+  :init
+  (evil-collection-init))
+
+(use-package magit
+  :ensure t)
+
+(use-package go-translate
+  :ensure t)
+
+(setq gts-translate-list '(("de" "en") ("jp" "de")))
+
+;; (setq gts-default-translator (gts-translator :engines (gts-bing-engine)))
+(setq gts-default-translator
+      (gts-translator
+       :picker (gts-prompt-picker)
+       :engines (list (gts-bing-engine) (gts-google-engine))
+       :render (gts-buffer-render)))
+
+(setq default-directory "~/orgdoc/")
 
 ;; general keybinds
 (use-package general
@@ -157,7 +189,19 @@
        "w n"   '(evil-window-new :which-key "create new window")
        "w r"   '(evil-window-move-far-right :which-key "moves window to the
        right")
-	   "t t"   '(term :which-key "opens a terminal"))
+	   "g t"   '(gts-do-translate :which-key "starts translator")
+	   "l h"   '((lambda() (interactive) (set-input-method "japanese-hiragana"))
+				 :which-key "changes input language to japanese hiragana")
+	   "l k"   '((lambda() (interactive) (set-input-method "japanese-katakana"))
+				 :which-key "changes input language to japanese katakana")
+	   "l j"   '((lambda() (interactive) (set-input-method "japanese"))
+				 :which-key "changes input language to japanese kanji")
+	   "l l"   '((lambda() (interactive) (set-input-method "latin-prefix"))
+				 :which-key "changes input language to latin-prefix
+       (default)")
+	   "k k"   '(kanji-mode-stroke-order :which-key "shows kanji stroke order")
+	   "t t"   '(term :which-key "opens a terminal")
+	   "o s"   '(shell :which-key "opens a shell"))
 
 (nvmap :states '(normal visual) :keymaps 'override :prefix "SPC"
        "e b"   '(eval-buffer :which-key "Eval elisp in buffer")
@@ -171,34 +215,40 @@
        "f f"   '(find-file :which-key "Find file")
        "f s"   '(save-buffer :which-key "Save file")
        "f u"   '(sudo-edit-find-file :which-key "Sudo find file")
-       "f y"   '(dt/show-and-copy-buffer-path :which-key "Yank file path")
-       "f C"   '(copy-file :which-key "Copy file")
-       "f D"   '(delete-file :which-key "Delete file")
-       "f R"   '(rename-file :which-key "Rename file")
-       "f S"   '(write-file :which-key "Save file as...")
-       "f U"   '(sudo-edit :which-key "Sudo edit file"))
+       "f c"   '(copy-file :which-key "Copy file")
+       "f d"   '(delete-file :which-key "Delete file")
+       "f r"   '(rename-file :which-key "Rename file")
+       "f w"   '(write-file :which-key "Save file as...")
+       "f S"   '(sudo-edit-find-file :which-key "Sudo edit file"))
 
 (nvmap :keymaps 'override :prefix "SPC"
        "c c"   '(compile :which-key "Compile")
-       "h r r" '((lambda () (interactive) (load-file "~/.emacs.d/init.el"))
+  	   "n n"   '((lambda() (interactive) (find-file "~/orgdoc/notes.org"))
+				 :which-key "access personal notes")
+  	   "e c c" '((lambda() (interactive) (find-file "~/.emacs.d/init.el"))
+				 :which-key "access emacs config")
+	   "h r r" '((lambda () (interactive) (load-file "~/.emacs.d/init.el"))
 				 :which-key "Reload emacs config"))
-       
+
 (nvmap :keymaps 'override :prefix "SPC"
-       "m *"   '(org-ctrl-c-star :which-key "Org-ctrl-c-star")
-       "m +"   '(org-ctrl-c-minus :which-key "Org-ctrl-c-minus")
-       "m e"   '(org-export-dispatch :which-key "Org export dispatch")
-       "m f"   '(org-footnote-new :which-key "Org footnote new")
-       "m h"   '(org-toggle-heading :which-key "Org toggle heading")
-       "m i"   '(org-toggle-item :which-key "Org toggle item")
-       "m n"   '(org-store-link :which-key "Org store link")
-       "m o"   '(org-set-property :which-key "Org set property")
-       "m t"   '(org-todo :which-key "Org todo")
-       "m x"   '(org-toggle-checkbox :which-key "Org toggle checkbox")
-       "m B"   '(org-babel-tangle :which-key "Org babel tangle")
-       "m I"   '(org-toggle-inline-images :which-key "Org toggle inline imager")
-       "m T"   '(org-todo-list :which-key "Org todo list")
+       "o *"   '(org-ctrl-c-star :which-key "Org-ctrl-c-star")
+       "o -"   '(org-ctrl-c-minus :which-key "Org-ctrl-c-minus")
+       "o e"   '(org-export-dispatch :which-key "Org export dispatch")
+       "o f"   '(org-footnote-new :which-key "Org footnote new")
+       "o h"   '(org-toggle-heading :which-key "Org toggle heading")
+       "o i"   '(org-toggle-item :which-key "Org toggle item")
+       "o n"   '(org-store-link :which-key "Org store link")
+       "o p"   '(org-set-property :which-key "Org set property")
+       "o t"   '(org-todo :which-key "Org todo")
+       "o x"   '(org-toggle-checkbox :which-key "Org toggle checkbox")
+       "o B"   '(org-babel-tangle :which-key "Org babel tangle")
+       "o I"   '(org-toggle-inline-images :which-key "Org toggle inline imager")
+       "o T"   '(org-todo-list :which-key "Org todo list")
        "o a"   '(org-agenda :which-key "Org agenda")
-       )
+       "o s"   '(org-skeleton :which-key "insert org skeleton template")
+	   "s e"   '(org-edit-src-exit :which-key "exit src code block edit")
+	   "s w"   '(org-edit-src-code :which-key "edit src code block")
+	   "s s"   '(org-edit-src-save :which-key "save src code edit"))
 
 (column-number-mode)
 (global-display-line-numbers-mode t)
@@ -208,6 +258,9 @@
                 eshell-mode-hook
 		shell-mode-hook))
 
+(use-package kanji-mode
+  :ensure t)
+  
 (use-package dashboard
   :init      ;; tweak dashboard config before loading it
   (setq dashboard-set-heading-icons t)
@@ -216,7 +269,7 @@
   ;;(setq dashboard-startup-banner 'logo) ;; use standard emacs logo as banner
   (setq dashboard-startup-banner "~/.emacs.d/monado_lambda_emacs.png")  ;; use custom image as banner
   (setq dashboard-center-content t) ;; set to 't' for centered content
-  (setq dashboard-items '((recents . 5)
+  (setq dashboard-items '((recents . 7)
                           (agenda . 5 )
                           (bookmarks . 3)))
   :config
@@ -231,7 +284,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(general which-key use-package pdf-tools org-bullets ivy evil doom-themes doom-modeline all-the-icons)))
+   '(kanji-mode ement magit-popup magit evil-collection anki go-translate cl-libify general which-key use-package pdf-tools org-bullets ivy evil doom-themes doom-modeline all-the-icons)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
